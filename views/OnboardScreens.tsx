8ace1c735ed4eae0513daeff840a89abaa2f5574
Weronika <weronikamazurczak202@gmail.com>
2023-11-30 21:17:23 +0100

    usuniecie node_modules

--- views/OnboardScreens.tsx
+++ views/OnboardScreens.tsx
@@ -1,28 +1,47 @@
-import { View, Image } from '@gluestack-ui/themed';
+import { View, Image, Text } from '@gluestack-ui/themed';
 import React from 'react'
 import Onboarding from 'react-native-onboarding-swiper';
+import styles from './styles';
+
 
 export default function OnboardScreens() {
   return (
-    <View style={{flex:1, alignItems:'center'}}>
+    <View style={styles.ladowanie}>
     <Onboarding 
     onDone={()=>{console.log('login')}}
 pages={[
   {
     backgroundColor: '#fff',
-    image: <Image source={require('../assets/favicon.png')} alt="test" />,
+    image: (
+      <View style={styles.kropk1}>
+        <Image style={styles.img} source={require('../assets/Start1.png')} alt="test" />
+      </View>
+    ),  
+    title:(
+        <Text style={styles.tytul}>
+          Bezpieczny i godny zaufania
+        </Text>
+    ), 
+    subtitle:(
+      <Text style={styles.podtytul}>
+        Poznaj naszą bankową aplikację, która zapewnia najwyższy poziom bezpieczeństwa i zaufania. Twoje finanse są u nas w dobrych rękach.
+      </Text>
+    ) 
+  },
+  {
+    backgroundColor: '#fff',
+    image:(
+      <View style={styles.kropk1}>
+         <Image style={styles.img} source={require('../assets/Start2.png')} alt="test" />
+         </View>
+    ),
+    
     title: 'Onboarding',
     subtitle: 'Done with React Native Onboarding Swiper',
   },
   {
     backgroundColor: '#fff',
-    image: <Image source={require('../assets/favicon.png')} alt="test" />,
-    title: 'Onboarding',
-    subtitle: 'Done with React Native Onboarding Swiper',
-  },
-  {
-    backgroundColor: '#fff',
-    image: <Image source={require('../assets/favicon.png')} alt="test" />,
+    image: <Image source={require('../assets/Start3.png')} alt="test" />,
     title: 'Onboarding',
     subtitle: 'Done with React Native Onboarding Swiper',
   },
---
+++ views/styles.tsx
@@ -0,0 +1,43 @@
+import { StyleSheet } from 'react-native';
+import React from 'react';
+
+
+const styles = StyleSheet.create({
+    ladowanie:{
+        flex: 1,
+
+    },
+    kropk1:{
+        justifyContent: 'center',
+   
+    },
+    img:{
+        height: 424,
+        width: 390,
+        marginTop: -90,
+        
+    },
+    tytul:{
+        fontSize: 35,
+        padding: 10,
+        fontWeight: 'bold',
+        color: '#000',
+        textAlign: 'center',
+        lineHeight: 40,
+        marginTop: -40,
+        marginBottom: 15,
+    },
+    podtytul:{
+        fontSize: 18,
+        padding: 10,
+        color: '#000',
+        lineHeight: 20,
+        marginTop: -20,
+        paddingLeft: 20,
+        paddingRight: 20,
+        letterSpacing: 1,
+
+    }
+});
+
+export default styles;
